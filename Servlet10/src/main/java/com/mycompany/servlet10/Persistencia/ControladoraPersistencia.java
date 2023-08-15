d52e5a19adba4c73f89a62aef18c962deb5e55e3
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-08-15 15:24:42 -0500

    Eliminando con Java web terminado

--- Servlet10/src/main/java/com/mycompany/servlet10/Persistencia/ControladoraPersistencia.java
+++ Servlet10/src/main/java/com/mycompany/servlet10/Persistencia/ControladoraPersistencia.java
@@ -2,7 +2,10 @@
 package com.mycompany.servlet10.Persistencia;
 
 import com.mycompany.servlet10.Logica.Usuarios;
+import com.mycompany.servlet10.Persistencia.exceptions.NonexistentEntityException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ControladoraPersistencia
 {
@@ -18,5 +21,17 @@ public class ControladoraPersistencia
     public  List<Usuarios> TraerUsuarios()
     {
         return  usuariosJpaController.findUsuariosEntities();
+    }
+
+    public void BorrarUsuario(int IdEliminar)
+    {
+        try
+        {
+            usuariosJpaController.destroy(IdEliminar);
+        }
+        catch (NonexistentEntityException ex)
+        {
+            Logger.getLogger(ControladoraPersistencia.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
