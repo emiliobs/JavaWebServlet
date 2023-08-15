1411027f0767a5515d35720f2bc832f795e45106
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-08-14 21:54:45 -0500

    Java Web con jsp y servlet y mas

---
+++ src/main/java/com/mycompany/servlet10/Logica/Usuarios.java
@@ -0,0 +1,65 @@
+
+package com.mycompany.servlet10.Logica;
+
+
+public class Usuarios
+{
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public Usuarios(String dni, String nombre, String apellido, String telefono)
+    {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public Usuarios()
+    {
+    }
+
+    public String getDni()
+    {
+        return dni;
+    }
+
+    public void setDni(String dni)
+    {
+        this.dni = dni;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    public String getApellido()
+    {
+        return apellido;
+    }
+
+    public void setApellido(String apellido)
+    {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono()
+    {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono)
+    {
+        this.telefono = telefono;
+    }
+    
+    
+}
