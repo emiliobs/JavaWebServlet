c6680a94e1b1dc47643f633c2cfe64f6981566aa
Emilio Barrera Sepúlveda <barrera_emilio@yahoo.es>
2023-08-15 22:56:22 -0500

    Editando usuarios listo y otras configuraciones

--- Servlet10/src/main/java/com/mycompany/servlet10/Logica/Controladora.java
+++ Servlet10/src/main/java/com/mycompany/servlet10/Logica/Controladora.java
@@ -22,4 +22,14 @@ public class Controladora
     {
         controladoraPersistencia.BorrarUsuario(IdEliminar);
     }
+
+    public Usuarios TraerUsuarioPorId(int IdEditar)
+    {
+       return controladoraPersistencia.TraerUsuarioPorId(IdEditar);
+    }
+
+    public void EditarUsuario(Usuarios usuariosEditar)
+    {
+        controladoraPersistencia.EditarUsuario(usuariosEditar);
+    }
 }
